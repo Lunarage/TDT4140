441b286b4e3226c58c6cecc11ebfe153753a1b70
Farhad1401 <54811161+Farhad1401@users.noreply.github.com>
2021-02-09 11:36:54 +0100

    #1 Implementert en organization og activity model

--- backend/gjorno/models.py
+++ backend/gjorno/models.py
@@ -3,6 +3,7 @@ These are the models.
 
 https://docs.djangoproject.com/en/3.1/topics/db/models/
 """
+from django.conf import settings
 from django.db import models
 
 
@@ -11,3 +12,23 @@ class Test(models.Model):
     A test model
     """
     value = models.CharField(max_length=80)
+
+
+class Organization(models.Model):
+    """
+    A Organization model
+    """
+    name = models.CharField(max_length=80, help_text="The name of the organization")
+
+
+class Activity(models.Model):
+    """
+    An Activity model
+    """
+    title = models.CharField(max_length=80)
+    date = models.DateTimeField(null=True, blank=True)
+    organization_owner = models.ForeignKey(Organization, on_delete=models.SET_NULL, null=True)
+    user_owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True)
+
+
+
