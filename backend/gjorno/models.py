509541810f0703d5356453e54902862870f6febf
Farhad1401 <54811161+Farhad1401@users.noreply.github.com>
2021-02-12 14:20:14 +0100

    #1 Updated readme and added a new model

--- backend/gjorno/models.py
+++ backend/gjorno/models.py
@@ -16,19 +16,32 @@ class Test(models.Model):
 
 class Organization(models.Model):
     """
-    A Organization model
+    A Organization model consisting of an attribute "name"
     """
     name = models.CharField(max_length=80, help_text="The name of the organization")
 
 
+class Category(models.Model):
+    """
+    A Categories model consisting of an attribute "name"
+    """
+    name = models.CharField(max_length=80)
+    
+
 class Activity(models.Model):
     """
-    An Activity model
+    An Activity model which consists of a tittle and the date to when the activity is going to happen.
+    An activity can be owned by a user or by an organization and can fall into spesific category. 
     """
     title = models.CharField(max_length=80)
     date = models.DateTimeField(null=True, blank=True)
     organization_owner = models.ForeignKey(Organization, on_delete=models.SET_NULL, null=True, blank=True)
     user_owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, blank=True)
+    activity_category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
+
+
+
+
 
 
 
