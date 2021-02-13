c2f34dcf961d9ee01587fb499a1d31c6b5e79518
Henrik Øberg Myhre <henrik.myhre99@gmail.com>
2021-02-13 18:01:38 +0100

    Welcome page design

--- frontend/src/welcome/Welcome.tsx
+++ frontend/src/welcome/Welcome.tsx
@@ -1,17 +1,30 @@
 import React from 'react'
+import styled from 'styled-components';
 import Button from '../components/Button';
+import WelcomeLogo from './WelcomeLogo';
 
+const PageWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+`;
 
-interface WelcomeProps {
-  loggedIn: boolean
-}
+const ContentWrapper = styled.div`
+  margin: 130px 100px 10px 10px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+`;
 
-const Welcome = ({ loggedIn }: WelcomeProps) => (
-  <div>
-    <div>hello world {loggedIn} </div>
-    {loggedIn && <div> logget inn </div>}
-    <Button text="button" />
-  </div>
+const Welcome = () => (
+  <PageWrapper>
+    <ContentWrapper>
+      <WelcomeLogo />
+      <Button text="Utforsk" />
+      <Button text="Logg inn" />
+    </ContentWrapper>
+  </PageWrapper>
 );
 
 export default Welcome
