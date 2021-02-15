623100f031f8e0662b77eb12d9113c86f517c31c
Thomas Heggø Teigland <thomasteigland@Thomass-MacBook-Air.local>
2021-02-15 13:13:26 +0100

    Button design

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -6,15 +6,36 @@ interface ButtonProps {
 }
 
 const CustomButton = styled.div`
-  width: 50px;
-  height: 50px;
-  border-style: solid;
-  border-color: red;
+  width: 75px;
+  height: 25px;
+  color: white;
+  margin: 10px;
+  background-color: #ff4e37;
+  padding: 3px 20px;
+  outline: 0;
+  transition: ease backgroundcolor 250ms;
+  border-radius: 40px;
+  font-weight: bold;
+  font-style: normal;
+  text-align: center;
+  display: table;
+
+  &:hover {
+    cursor: pointer;
+    background-color: #dc3f2a;
+  }
+`;
+
+const TextWrapper = styled.div`
+  display: table-cell;
+  vertical-align: middle;
 `;
 
 const Button = ({ text }: ButtonProps) => (
   <CustomButton>
-    text
+    <TextWrapper>
+      { text }
+    </TextWrapper>
   </CustomButton>
 );
 
