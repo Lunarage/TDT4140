eb3cbd8a7bb8fe94c8405eb70dc3eadbdc0ba2dc
Henrik Øberg Myhre <henrik.myhre99@gmail.com>
2021-02-23 00:55:42 +0100

    Browse and view events

--- frontend/src/components/ActivityExpand.tsx
+++ frontend/src/components/ActivityExpand.tsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components';
 import { redHexColor } from '../consts';
+import { isoToDayMonthYear } from '../functions';
+import { Event } from '../store/types';
 
 const Wrapper = styled.div`
   background-color: ${redHexColor};
-  width: 1000px;
-  height: 770px;
+  width: auto;
+  min-width: 800px;
+  height: auto;
+  min-height: 500px;
   box-shadow: 1px 1px 20px 4px rgba(0, 0, 0, 0.25);
   color: white;
   font-style: normal;
@@ -32,17 +36,18 @@ const ActivityExpandHeader = styled.h1`
 `;
 
 const Content = styled.div`
-  height: 617px;
+  height: 550px;
   width: 100%;
   color: white;
   display: flex;
   justify-content: space-start;
   font-size: 14px;
   line-height: 20px;
+  padding: 20px;
 `;
 
 const TextContentWrapper = styled.div`
-  width: 702px;
+  width: auto;
   display: flex;
   flex-direction: column;
   padding: 30px;
@@ -50,11 +55,12 @@ const TextContentWrapper = styled.div`
 
 const TextContent = styled.div`
   width: 100%;
+  max-width: 600px;
 `;
 
 
 const Image = styled.img`
-    height: 617px;
+    height: 100%;
     top: 27px;
     margin-left: -50%;
 `;
@@ -63,7 +69,6 @@ const ImageWrapper = styled.div`
     width: 298px;
     height: 100%;
     overflow: hidden;
-    margin: 20px;
     box-shadow: 1px 1px 20px 4px rgba(0, 0, 0, 0.25);
 `;
 
@@ -94,49 +99,56 @@ const CloseButton = styled.button`
 `;
 
 interface ActivityExpandProps {
+  data: Event
   onExitFunc: () => void;
 }
 
-const ActivityExpand = ({ onExitFunc }: ActivityExpandProps) => (
-  <Wrapper>
-    <CloseButton onClick={onExitFunc} >X </CloseButton>
-    <ActivityExpandHeader>
-      &nbsp; &nbsp; SPACEWALK
-         <Logo src="static/NASA_logo.png" />
-    </ActivityExpandHeader>
+const ActivityExpand = ({ data, onExitFunc }: ActivityExpandProps) => {
+  const [year, month, day] = isoToDayMonthYear(data.date)
 
-    <Content>
-      <ImageWrapper> <Image src="static/22858269.jpg" />
-      </ImageWrapper>
+  return (
+    <Wrapper>
+      <CloseButton onClick={onExitFunc} >X </CloseButton>
+      {data.title ? <ActivityExpandHeader>
+        &nbsp; &nbsp; {data.title}
+      </ActivityExpandHeader> : <ActivityExpandHeader>
+          &nbsp; &nbsp; EVENT
+    </ActivityExpandHeader>}
 
-      <TextContentWrapper>
+      <Content>
+        <ImageWrapper> <Image src="static/22858269.jpg" />
+        </ImageWrapper>
 
-        <TextContent>
-          <br></br><b>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis nec ligula velit. Suspendisse lectus sem, dictum ultricies diam tincidunt, imperdiet porttitor dolor. Suspendisse pretium ex vel felis interdum, id ornare sem finibus.</b>
-        </TextContent>
+        <TextContentWrapper>
 
-        <TextContent>
-          <br></br>
-          <h2>Utendørs</h2>
-          <h2>Sted: Interstellar</h2>
-          <h2>Dato: 12. Februar 2021</h2>
-          <h2>Klokkeslett: 14.00</h2>
-        </TextContent>
+          <TextContent>
+            <br></br>{data.description && <b>{data.description}</b>}
+          </TextContent>
 
-        <TextContent>
-          <br></br>
-          <div>Intensitet - 10</div>
-          <div>Kategori - Sport og friluftsliv</div>
-          <div>Antall plasser - 4</div>
-        </TextContent>
+          <TextContent>
+            <br></br>
+            {/* <h2>Utendørs</h2> */}
+            {data.location && <h2>Sted: {data.location}</h2>}
+            {year && month && day && <h2>Dato: {day}/{month} {year}</h2>}
+            {/* <h2>Klokkeslett: 14.00</h2> */}
+          </TextContent>
 
-        <TextContent>
-          <br></br>
-          <h2>Om organisasjonen</h2>
-          <div>National Aeronautics and Space Administration (NASA) er en amerikansk føderal etat med oppgaver knyttet til romfart og luftfart. Etaten ble opprettet i 1958 som en direkte følge av Sovjetunionens oppskytning av Sputnik 1. NASA har omkring 18 000 ansatte og hovedkontor i Washington, DC. NASA står bak store bragder, for eksempel Apollo-programmet som sendte mennesker til månen for første gang. NASA sto også bak Mercury-programmet og Gemini-programmet.</div>
-        </TextContent>
-      </TextContentWrapper>
-    </Content>
-  </Wrapper>
-);
+          <TextContent>
+            <br></br>
+            {data.activity_level && <div>Intensitet - {data.activity_level}</div>}
+            {data.categories && <div>Kategori - {data.categories.toString()}</div>}
+            {data.max_participants && <div>Antall plasser - {data.max_participants}</div>}
+          </TextContent>
+
+          {/* <TextContent>
+            <br></br>
+            {<h2>Om organisasjonen</h2>
+            <div>National Aeronautics and Space Administration (NASA) er en amerikansk føderal etat med oppgaver knyttet til romfart og luftfart. Etaten ble opprettet i 1958 som en direkte følge av Sovjetunionens oppskytning av Sputnik 1. NASA har omkring 18 000 ansatte og hovedkontor i Washington, DC. NASA står bak store bragder, for eksempel Apollo-programmet som sendte mennesker til månen for første gang. NASA sto også bak Mercury-programmet og Gemini-programmet.</div> 
+          </TextContent>*/}
+        </TextContentWrapper>
+      </Content>
+    </Wrapper>
+  );
+}
+
 export default ActivityExpand
